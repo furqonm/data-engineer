18917e9f8b6ddf5ca57c649bd9de5a42075f955b
Furqon Mauladani <furqonmauladani@gmail.com>
2025-08-19 00:22:27 +0700

    Delete setup.py

--- dataflow/setup.py
+++
@@ -1,9 +0,0 @@
-# setup.py
-import setuptools
-
-setuptools.setup(
-    name='my-dataflow-job',
-    version='1.0.0',
-    install_requires=['apache-beam[gcp]==2.66.0'],  # Use a recent version here
-    packages=setuptools.find_packages()
-)
