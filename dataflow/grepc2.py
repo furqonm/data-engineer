feb34f55143254d4ab729da8504dc1d55251b317
Furqon Mauladani <furqonmauladani@gmail.com>
2025-08-19 06:15:15 +0700

    Update grepc2.py

--- dataflow/grepc2.py
+++ dataflow/grepc2.py
@@ -8,18 +8,14 @@ for each vendor, and writes the results to another GCS bucket.
 
 import apache_beam as beam
 
-# Fungsi untuk memparsing dan membuang header
 def parse_and_filter_csv(line):
-    # Buang header
     if not line.startswith('vendor_id'):
         parts = line.split(',')
         try:
             vendor_id = parts[0]
             total_amount = float(parts[14])
-            # Menggunakan yield untuk menghasilkan elemen valid
             yield (vendor_id, total_amount)
         except (ValueError, IndexError):
-            # Abaikan baris yang rusak atau tidak valid
             pass
 
 PROJECT = 'qwiklabs-gcp-03-396637544609'
@@ -29,22 +25,27 @@ REGION = 'europe-west4'
 def run():
     argv = [
       '--project={0}'.format(PROJECT),
-      '--job_name=lab-aggregate-csv',
+      '--job_name=aggregate-csv',
       '--save_main_session',
       '--staging_location=gs://{0}/staging/'.format(BUCKET),
       '--temp_location=gs://{0}/staging/'.format(BUCKET),
       '--region={0}'.format(REGION),
-      '--worker_machine_type=e2-standard-2',
       '--runner=DataflowRunner',
-      '--sdk_container_image=gcr.io/cloud-dataflow/python/flex-image-py310-apache-beam-2.66.0:latest'
+      '--worker_machine_type=e2-standard-2',
+      '--sdk_container_image=gcr.io/cloud-dataflow/python/flex-image-py310-apache-beam-2.66.0:latest',
+      '--experiments=use_runner_v2',
+      '--sdk_location=container'
     ]
+
+    # Important: Ensure your local environment has the same SDK version
+    # Before running the script, execute this command in your terminal:
+    # pip install apache-beam[gcp]==2.66.0
 
     p = beam.Pipeline(argv=argv)
     
     input_file = 'gs://cloud-training/OCBL013/nyc_tlc_yellow_trips_2018_subset_1.csv'
     output_prefix = 'gs://{0}/csv_aggregation/output'.format(BUCKET)
 
-    # Membaca data CSV dan melakukan agregasi
     (p 
         | 'GetCSV' >> beam.io.ReadFromText(input_file)
         | 'ParseAndFilter' >> beam.FlatMap(parse_and_filter_csv)
